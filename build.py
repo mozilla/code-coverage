c73df5e403b662cae18e8a2e0175f33659483fe3
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-21 12:07:55 +0100

    Build a file with the supported extensions retrieved from the backend

--- build.py
+++ build.py
@@ -1,12 +1,20 @@
 import os
 import zipfile
 
+import requests
+
+
 zip_file = 'gecko-code-coverage.zip'
 
 try:
     os.remove(zip_file)
 except FileNotFoundError:
     pass
+
+r = requests.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_extensions')
+r.raise_for_status()
+with open('supported_extensions.js', 'w') as f:
+    f.write('const SUPPORTED_EXTENSIONS = {};'.format(r.text.rstrip('\n')))
 
 files = [f for f in os.listdir('.') if os.path.isfile(f) and not f.endswith('.py')]
 for f in ['.gitignore', '.travis.yml', 'LICENSE', 'README.md', 'package.json', 'package-lock.json']:
