5219bfd08d3eddb5e06d2721adb8cc164320feaa
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-21 12:33:59 +0100

    Rename variable to make flake8 happy

--- build.py
+++ build.py
@@ -15,8 +15,8 @@ except OSError as e:
 
 r = requests.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_extensions')
 r.raise_for_status()
-with open('supported_extensions.js', 'w') as f:
-    f.write('const SUPPORTED_EXTENSIONS = {};'.format(r.text.rstrip('\n')))
+with open('supported_extensions.js', 'w') as se_f:
+    se_f.write('const SUPPORTED_EXTENSIONS = {};'.format(r.text.rstrip('\n')))
 
 files = [f for f in os.listdir('.') if os.path.isfile(f) and not f.endswith('.py')]
 for f in ['.gitignore', '.travis.yml', 'LICENSE', 'README.md', 'package.json', 'package-lock.json', 'requirements.txt', 'setup.cfg']:
