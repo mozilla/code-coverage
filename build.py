eb345d6525c5d635e9cf8ef008e688f5e879689b
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-21 11:25:35 +0100

    Run linting on Travis

--- build.py
+++ build.py
@@ -9,7 +9,7 @@ except FileNotFoundError:
     pass
 
 files = [f for f in os.listdir('.') if os.path.isfile(f) and not f.endswith('.py')]
-for f in ['.gitignore', 'LICENSE', 'README.md', 'package.json', 'package-lock.json']:
+for f in ['.gitignore', '.travis.yml', 'LICENSE', 'README.md', 'package.json', 'package-lock.json']:
     files.remove(f)
 
 with zipfile.ZipFile(zip_file, mode='w', compression=zipfile.ZIP_DEFLATED) as z:
