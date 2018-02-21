c414665be78bb38504f1dc8a99ff4f1629de3290
Marco Castelluccio <mcastelluccio@mozilla.com>
2018-02-21 12:27:27 +0100

    Use Python 2 to make Travis happy easily

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+import errno
 import os
 import zipfile
 
@@ -8,8 +9,9 @@ zip_file = 'gecko-code-coverage.zip'
 
 try:
     os.remove(zip_file)
-except FileNotFoundError:
-    pass
+except OSError as e:
+    if e.errno != errno.ENOENT:
+        raise
 
 r = requests.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_extensions')
 r.raise_for_status()
@@ -17,7 +19,7 @@ with open('supported_extensions.js', 'w') as f:
     f.write('const SUPPORTED_EXTENSIONS = {};'.format(r.text.rstrip('\n')))
 
 files = [f for f in os.listdir('.') if os.path.isfile(f) and not f.endswith('.py')]
-for f in ['.gitignore', '.travis.yml', 'LICENSE', 'README.md', 'package.json', 'package-lock.json']:
+for f in ['.gitignore', '.travis.yml', 'LICENSE', 'README.md', 'package.json', 'package-lock.json', 'requirements.txt', 'setup.cfg']:
     files.remove(f)
 
 with zipfile.ZipFile(zip_file, mode='w', compression=zipfile.ZIP_DEFLATED) as z:
