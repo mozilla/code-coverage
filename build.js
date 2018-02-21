9b1d7db425470491850d3aa718947e2a8764748f
Streanga Sarmis <streangasarmis@gmail.com>
2018-02-21 22:12:17 +0200

    Specify which files need to be included in the ZIP archive, to avoid including extraneous files (#19)

--- build.js
+++ build.js
@@ -4,6 +4,14 @@ const fs = require('fs');
 const https = require('https');
 const archiver = require('archiver');
 
+const includeFiles = [
+  'bugzilla.js', 'coverage.jpg', 'coverage.js',
+  'dxr.css', 'dxr.js', 'dxr-common.js',
+  'manifest.json', 'mozreview.js', 'searchfox.js',
+  'spinner.css', 'supported_extensions.js',
+];
+const zipName = './gecko-code-coverage.zip';
+
 https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_extensions', res => {
   let data = '';
     
@@ -11,16 +19,11 @@ https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_e
   res.on('end', () => {
     let content = `const SUPPORTED_EXTENSIONS = ${data};`;
 
-    fs.writeFile("supported_extensions.js", content, e => {
+    fs.writeFile('supported_extensions.js', content, e => {
       if (e) {
         console.error(e.message);
         return;
       }
-
-      let excludeFiles = [
-        ".gitignore", ".travis.yml", "LICENSE", "README.md",
-        "package.json", "package-lock.json", "build.js",
-      ];
 
       fs.readdir(".", (e, files) => {
         if (e) {
@@ -28,7 +31,7 @@ https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_e
           return;
         }
 
-        const resultFiles = files.filter(file => !excludeFiles.includes(file));
+        const resultFiles = files.filter(file => includeFiles.includes(file));
 
         makeZip(resultFiles);
       });
@@ -37,7 +40,7 @@ https.get('https://uplift.shipit.staging.mozilla-releng.net/coverage/supported_e
 }).on('error', e => console.error(e.message));
 
 function makeZip(list){
-  const output = fs.createWriteStream('./gecko-code-coverage.zip');
+  const output = fs.createWriteStream(zipName);
 
   let archive = archiver('zip', {
       zlib: { level: 9 }
