11536912bce55629df04c85210a2e5e1e05741d7
Assiya Khuzyakhmetova <assiya.khuzyakhmetova@nu.edu.kz>
2018-05-29 23:33:22 +0600

    changed name (#26)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='firefox-code-coverage',
+    name='firefox_code_coverage',
     version='1.0.0',
     description='Code Coverage Report generator for Firefox',
     packages=find_packages(exclude=['contrib', 'docs', 'tests'])
