f1025f12df6d1cbce23e8e3a133077f4726598a6
Bastien Abadie <bastien@nextcairn.com>
2019-07-17 16:07:20 +0200

    Allow using other secrets in run.sh

--- backend/run.sh
+++ backend/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-export TASKCLUSTER_SECRET="project/relman/code-coverage/dev"
+if [[ ! $TASKCLUSTER_SECRET ]]; then
+  export TASKCLUSTER_SECRET="project/relman/code-coverage/dev"
+  echo 'Using dev secret'
+fi
 gunicorn --bind localhost:8000 --reload --reload-engine=poll --log-file=- code_coverage_backend.flask:app
