74ae9f0998085d218ca05549d5a8f058fb00350e
Bastien Abadie <bastien@nextcairn.com>
2019-07-22 15:30:09 +0200

    Add restart tasks script.

---
+++ bot/tools/restart_tasks.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import argparse
+import os
+
+from taskcluster.utils import slugId
+
+from code_coverage_bot.secrets import secrets
+from code_coverage_bot.taskcluster import taskcluster_config
+
+CODECOV_URL = 'https://codecov.io/api/gh/marco-c/gecko-dev/commit'
+MC_REPO = 'https://hg.mozilla.org/mozilla-central'
+HOOK_GROUP = 'project-relman'
+HOOK_ID = 'code-coverage-{app_channel}'
+
+taskcluster_config.auth(
+    os.environ.get('TASKCLUSTER_CLIENT_ID'),
+    os.environ.get('TASKCLUSTER_ACCESS_TOKEN'),
+)
+secrets.load(
+    os.environ['TASKCLUSTER_SECRET'],
+)
+queue = taskcluster_config.get_service('queue')
+
+
+def list_commits(tasks, existing=[]):
+    '''
+    Read the revision from an existing code coverage task
+    '''
+    for task_id in tasks:
+        try:
+            task = queue.task(task_id)
+            yield task['payload']['env']['REVISION']
+        except Exception as e:
+            print('Failed to load task {}: {}'.format(task_id, e))
+
+
+def trigger_task(task_group_id, commit):
+    '''
+    Trigger a code coverage task to build covdir at a specified revision
+    '''
+    assert isinstance(commit, str)
+    name = 'covdir {} - {}'.format(secrets[secrets.APP_CHANNEL], commit)
+    hooks = taskcluster_config.get_service('hooks')
+    payload = {
+        'REPOSITORY': MC_REPO,
+        'REVISION': commit,
+        'taskGroupId': task_group_id,
+        'taskName': name,
+    }
+    hook_id = HOOK_ID.format(app_channel=secrets[secrets.APP_CHANNEL])
+    return hooks.triggerHook(HOOK_GROUP, hook_id, payload)
+
+
+def main():
+    # CLI args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nb-tasks', type=int, default=5, help='NB of tasks to create')
+    parser.add_argument('--group', type=str, default=slugId(), help='Task group to create/update')
+    parser.add_argument('--dry-run', action='store_true', default=False, help='List actions without triggering any new task')
+    parser.add_argument('tasks', nargs='+', help='Existing tasks to retrigger')
+    args = parser.parse_args()
+
+    # List existing tags & commits
+    print('Group', args.group)
+    try:
+        group = queue.listTaskGroup(args.group)
+        commits = [
+            task['task']['payload']['env']['REVISION']
+            for task in group['tasks']
+            if task['status']['state'] not in ('failed', 'exception')
+        ]
+        print('Found {} commits processed in task group {}'.format(len(commits), args.group))
+    except Exception as e:
+        print('Invalid task group : {}'.format(e))
+        commits = []
+
+    # Trigger a task for each commit
+    for commit in list_commits(args.tasks, commits):
+        print('Triggering commit {}'.format(commit))
+        if args.dry_run:
+            print('>>> No trigger on dry run')
+        else:
+            out = trigger_task(args.group, commit)
+            print('>>>', out['status']['taskId'])
+
+        commits.append(commit)
+        if len(commits) > args.nb_tasks:
+            print('Max nb tasks reached !')
+            break
+
+
+if __name__ == '__main__':
+    main()
