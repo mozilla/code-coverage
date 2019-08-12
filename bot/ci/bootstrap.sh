37f55f085392349ed78dc2aa54ce3ea9c29df8b6
Bastien Abadie <bastien@nextcairn.com>
2019-08-12 19:39:05 +0200

    bot: Use mercurial 5.1

--- bot/ci/bootstrap.sh
+++ bot/ci/bootstrap.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -ex
 GRCOV_VERSION="v0.5.3"
-MERCURIAL_VERSION="4.8"
+MERCURIAL_VERSION="5.1"
 
 apt-get update
-apt-get install --no-install-recommends -y mercurial=4.8.* curl lcov bzip2
+apt-get install --no-install-recommends -y curl lcov bzip2 python2-minimal python-bz2file python-dev
+
+# Setup mercurial from its own website, without install pip2 which has a lot of dependencies
+curl -L https://www.mercurial-scm.org/release/mercurial-$MERCURIAL_VERSION.tar.gz | tar -C /opt -xvz
+cd /opt/mercurial-$MERCURIAL_VERSION
+python2 setup.py install
 
 # Setup grcov
 curl -L https://github.com/mozilla/grcov/releases/download/$GRCOV_VERSION/grcov-linux-x86_64.tar.bz2 | tar -C /usr/bin -xjv
