7e96df1cad5abb997b7d3aa77104fd7836bbb775
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-09-03 13:41:04 +0200

    Uninstall no longer needed packages after they have been used

--- bot/ci/bootstrap.sh
+++ bot/ci/bootstrap.sh
@@ -20,6 +20,7 @@ hg clone -r $VERSION_CONTROL_TOOLS_REV https://hg.mozilla.org/hgcustom/version-c
 ln -s /src/bot/ci/hgrc $HOME/.hgrc
 
 # Cleanup
+apt-get purge -y curl python-dev
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
 rm -rf /src/version-control-tools/.hg /src/version-control-tools/ansible /src/version-control-tools/docs /src/version-control-tools/testing
