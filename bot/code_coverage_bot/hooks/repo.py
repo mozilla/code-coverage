ef77462d98a80ecb28d02d25d75c72e89b521316
Bastien Abadie <bastien@nextcairn.com>
2019-09-12 15:57:25 +0200

    Update bot/code_coverage_bot/hooks/repo.py
    Co-Authored-By: Marco <mcastelluccio@mozilla.com>

--- bot/code_coverage_bot/hooks/repo.py
+++ bot/code_coverage_bot/hooks/repo.py
@@ -53,7 +53,7 @@ class RepositoryHook(Hook):
 
     def run(self):
         # Check the covdir report does not already exists
-        if uploader.gcp_covdir_exists(self.branch, self.revision, "all", "all"):
+        if self.config["gcp_upload"] and uploader.gcp_covdir_exists(self.branch, self.revision, "all", "all"):
             logger.warn("Full covdir report already on GCP")
             return
 
