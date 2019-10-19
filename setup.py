8957f960345c3d8adea782cfc9de7d5d4aa1146a
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-10-19 18:39:24 +0100

    Use pre-commit

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='firefox_code_coverage',
-    version='1.0.0',
-    description='Code Coverage Report generator for Firefox',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    license='MPL2',
+    name="firefox_code_coverage",
+    version="1.0.0",
+    description="Code Coverage Report generator for Firefox",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    license="MPL2",
 )
--- tests/test.py
+++ tests/test.py
@@ -15,10 +15,10 @@ from firefox_code_coverage import codecoverage
 class Test(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
-        for d in ['report', 'ccov-artifacts', 'lcov', 'lcov-bin']:
+        for d in ["report", "ccov-artifacts", "lcov", "lcov-bin"]:
             shutil.rmtree(d, ignore_errors=True)
 
-        for f in ['grcov', 'grcov_ver', 'output.info']:
+        for f in ["grcov", "grcov_ver", "output.info"]:
             try:
                 os.remove(f)
             except OSError as e:
@@ -30,100 +30,109 @@ class Test(unittest.TestCase):
         self.assertTrue(task_id)
 
         task_data = codecoverage.get_task_details(task_id)
-        self.assertEqual(task_data['metadata']['name'], 'build-linux64-ccov/debug')
+        self.assertEqual(task_data["metadata"]["name"], "build-linux64-ccov/debug")
 
-        revision = task_data['payload']['env']['GECKO_HEAD_REV']
-        task_id_2 = codecoverage.get_task('mozilla-central', revision)
+        revision = task_data["payload"]["env"]["GECKO_HEAD_REV"]
+        task_id_2 = codecoverage.get_task("mozilla-central", revision)
         self.assertEqual(task_id, task_id_2)
 
         artifacts = codecoverage.get_task_artifacts(task_id)
         chosen_artifact = None
         for artifact in artifacts:
-            if artifact['name'] == 'public/build/target.txt':
+            if artifact["name"] == "public/build/target.txt":
                 chosen_artifact = artifact
         self.assertIsNotNone(chosen_artifact)
 
-        tasks = codecoverage.get_tasks_in_group(task_data['taskGroupId'])
+        tasks = codecoverage.get_tasks_in_group(task_data["taskGroupId"])
         self.assertIsInstance(tasks, list)
 
         try:
-            os.mkdir('ccov-artifacts')
+            os.mkdir("ccov-artifacts")
         except OSError as e:
             if e.errno != errno.EEXIST:
                 raise e
 
-        codecoverage.download_artifact(task_id, chosen_artifact, 'ccov-artifacts')
-        self.assertTrue(os.path.exists('ccov-artifacts/%s_target.txt' % task_id))
-        os.remove('ccov-artifacts/%s_target.txt' % task_id)
+        codecoverage.download_artifact(task_id, chosen_artifact, "ccov-artifacts")
+        self.assertTrue(os.path.exists("ccov-artifacts/%s_target.txt" % task_id))
+        os.remove("ccov-artifacts/%s_target.txt" % task_id)
 
-        artifact_paths = codecoverage.download_coverage_artifacts(task_id, 'cppunit', None, 'ccov-artifacts')
-        self.assertEqual(len([a for a in os.listdir('ccov-artifacts') if 'grcov' in a]), 2)
-        self.assertEqual(len([a for a in os.listdir('ccov-artifacts') if 'jsvm' in a]), 2)
-        self.assertEqual(len([a for a in artifact_paths if 'grcov' in a]), 2)
-        self.assertEqual(len([a for a in artifact_paths if 'jsvm' in a]), 2)
+        artifact_paths = codecoverage.download_coverage_artifacts(
+            task_id, "cppunit", None, "ccov-artifacts"
+        )
+        self.assertEqual(
+            len([a for a in os.listdir("ccov-artifacts") if "grcov" in a]), 2
+        )
+        self.assertEqual(
+            len([a for a in os.listdir("ccov-artifacts") if "jsvm" in a]), 2
+        )
+        self.assertEqual(len([a for a in artifact_paths if "grcov" in a]), 2)
+        self.assertEqual(len([a for a in artifact_paths if "jsvm" in a]), 2)
 
         codecoverage.download_grcov()
-        codecoverage.generate_report('./grcov', 'lcov', 'output.info', artifact_paths)
-        self.assertTrue(os.path.exists('output.info'))
+        codecoverage.generate_report("./grcov", "lcov", "output.info", artifact_paths)
+        self.assertTrue(os.path.exists("output.info"))
 
         codecoverage.download_genhtml()
-        codecoverage.generate_html_report('tests', silent=True)
-        self.assertTrue(os.path.isdir('report'))
+        codecoverage.generate_html_report("tests", silent=True)
+        self.assertTrue(os.path.isdir("report"))
 
     def test_suite_name_from_task_name(self):
         cases = [
-            ('test-linux64-ccov/debug-gtest', 'gtest'),
-            ('test-linux64-ccov/debug-jsreftest-1', 'jsreftest'),
-            ('test-linux64-ccov/debug-mochitest-devtools-chrome-e10s-10', 'mochitest-devtools-chrome'),
-            ('test-linux64-ccov/debug-mochitest-clipboard', 'mochitest-clipboard'),
-            ('test-linux64-ccov/debug-reftest-no-accel-e10s-5', 'reftest-no-accel'),
-            ('test-linux64-ccov/debug-mochitest-5', 'mochitest'),
-            ('test-windows10-64-ccov/debug-mochitest-5', 'mochitest'),
-            ('test-windows10-64-ccov/debug-cppunit', 'cppunit'),
+            ("test-linux64-ccov/debug-gtest", "gtest"),
+            ("test-linux64-ccov/debug-jsreftest-1", "jsreftest"),
+            (
+                "test-linux64-ccov/debug-mochitest-devtools-chrome-e10s-10",
+                "mochitest-devtools-chrome",
+            ),
+            ("test-linux64-ccov/debug-mochitest-clipboard", "mochitest-clipboard"),
+            ("test-linux64-ccov/debug-reftest-no-accel-e10s-5", "reftest-no-accel"),
+            ("test-linux64-ccov/debug-mochitest-5", "mochitest"),
+            ("test-windows10-64-ccov/debug-mochitest-5", "mochitest"),
+            ("test-windows10-64-ccov/debug-cppunit", "cppunit"),
         ]
         for c in cases:
             self.assertEqual(codecoverage.get_suite(c[0]), c[1])
 
     def test_download_grcov(self):
         codecoverage.download_grcov()
-        self.assertTrue(os.path.exists('grcov'))
-        self.assertTrue(os.path.exists('grcov_ver'))
+        self.assertTrue(os.path.exists("grcov"))
+        self.assertTrue(os.path.exists("grcov_ver"))
 
-        with open('grcov_ver', 'r') as f:
+        with open("grcov_ver", "r") as f:
             ver = f.read()
 
         # grcov is downloaded again if the executable doesn't exist.
-        os.remove('grcov')
+        os.remove("grcov")
         codecoverage.download_grcov()
-        self.assertTrue(os.path.exists('grcov'))
-        self.assertTrue(os.path.exists('grcov_ver'))
+        self.assertTrue(os.path.exists("grcov"))
+        self.assertTrue(os.path.exists("grcov_ver"))
 
         # grcov isn't downloaded again if the executable exists and the version is the same.
-        with open('grcov', 'w') as f:
-            f.write('prova')
+        with open("grcov", "w") as f:
+            f.write("prova")
 
         codecoverage.download_grcov()
 
-        with open('grcov', 'r') as f:
-            self.assertEqual('prova', f.read())
+        with open("grcov", "r") as f:
+            self.assertEqual("prova", f.read())
 
         # grcov is overwritten if the version changes.
-        with open('grcov_ver', 'w') as f:
-            f.write('v0.0.0')
+        with open("grcov_ver", "w") as f:
+            f.write("v0.0.0")
 
         codecoverage.download_grcov()
 
-        self.assertTrue(os.path.getsize('grcov') > 5)
-        with open('grcov_ver', 'r') as f:
+        self.assertTrue(os.path.getsize("grcov") > 5)
+        with open("grcov_ver", "r") as f:
             self.assertEqual(ver, f.read())
 
     def test_download_genhtml(self):
         codecoverage.download_genhtml()
-        self.assertTrue(os.path.exists('./lcov-bin/usr/local/bin/genhtml'))
+        self.assertTrue(os.path.exists("./lcov-bin/usr/local/bin/genhtml"))
 
         codecoverage.download_genhtml()
-        self.assertTrue(os.path.exists('./lcov-bin/usr/local/bin/genhtml'))
+        self.assertTrue(os.path.exists("./lcov-bin/usr/local/bin/genhtml"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
