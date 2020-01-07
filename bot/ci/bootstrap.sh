fbef2590182c0b6f20a2235351f5977fc0ed3524
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:16:33 +0100

    Install bzip2 as it's needed to extract grcov, but uninstall it at the end of the script

--- bot/ci/bootstrap.sh
+++ bot/ci/bootstrap.sh
@@ -4,7 +4,7 @@ MERCURIAL_VERSION="5.2"
 VERSION_CONTROL_TOOLS_REV="102106f53cb2"
 
 apt-get update
-apt-get install --no-install-recommends -y gcc curl python-dev
+apt-get install --no-install-recommends -y gcc curl bzip2 python-dev
 
 pip install --disable-pip-version-check --quiet --no-cache-dir mercurial==$MERCURIAL_VERSION
 
@@ -17,7 +17,7 @@ hg clone -r $VERSION_CONTROL_TOOLS_REV https://hg.mozilla.org/hgcustom/version-c
 ln -s /src/bot/ci/hgrc $HOME/.hgrc
 
 # Cleanup
-apt-get purge -y gcc curl python-dev
+apt-get purge -y gcc curl bzip2 python-dev
 apt-get autoremove -y
 rm -rf /var/lib/apt/lists/*
 rm -rf /src/version-control-tools/.hg /src/version-control-tools/ansible /src/version-control-tools/docs /src/version-control-tools/testing
