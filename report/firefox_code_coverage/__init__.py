a86c06a3911bfb72da991a1b9346ceb79b7c8c34
Bastien Abadie <bastien@nextcairn.com>
2020-01-24 09:49:18 +0100

    Force usage of external TC url - no proxy

--- report/firefox_code_coverage/__init__.py
+++ report/firefox_code_coverage/__init__.py
@@ -3,3 +3,6 @@
 from taskcluster.helper import TaskclusterConfig
 
 taskcluster = TaskclusterConfig("https://firefox-ci-tc.services.mozilla.com")
+
+# Force root url to avoid proxy as grcov is not available on Community instance
+taskcluster.options = {"rootUrl": taskcluster.default_url}
