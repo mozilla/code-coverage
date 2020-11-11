330be230560ec455a5d486af6456cc5e7bb9cd81
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-12 00:23:17 +0100

    Fix importing webpack-merge after latest webpack release

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
