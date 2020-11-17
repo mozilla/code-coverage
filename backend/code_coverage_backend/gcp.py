74ffce078e1095548d67ff5c88d3290bc9e272ec
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-17 18:05:31 +0100

    backend, tools: Move some of the code to handle gcp reports from the backend to tools
    So it can more easily be used in the bot too

--- backend/code_coverage_backend/gcp.py
+++ backend/code_coverage_backend/gcp.py
@@ -2,15 +2,13 @@
 import calendar
 import math
 import os
-import re
 import tempfile
 from datetime import datetime
-from datetime import timedelta
+from typing import List
+from typing import Optional
 
-import pytz
 import redis
 import structlog
-import zstandard as zstd
 from dateutil.relativedelta import relativedelta
 
 from code_coverage_backend import covdir
@@ -19,7 +17,9 @@ from code_coverage_backend.hgmo import hgmo_pushes
 from code_coverage_backend.hgmo import hgmo_revision_details
 from code_coverage_backend.report import DEFAULT_FILTER
 from code_coverage_backend.report import Report
+from code_coverage_tools.gcp import download_report
 from code_coverage_tools.gcp import get_bucket
+from code_coverage_tools.gcp import list_reports
 
 logger = structlog.get_logger(__name__)
 __cache = None
@@ -77,7 +77,7 @@ class GCPCache(object):
         # Load most recent reports in cache
         for repo in REPOSITORIES:
             for report in self.list_reports(repo, nb=1):
-                self.download_report(report)
+                download_report(self.reports_dir, self.bucket, report.name)
 
     def ingest_pushes(self, repository, platform, suite, min_push_id=None, nb_pages=3):
         """
@@ -113,14 +113,12 @@ class GCPCache(object):
                     # Only ingest first report found in a push in order to stay below 30s response time
                     ingested = True
 
-    def ingest_report(self, report):
+    def ingest_report(self, report: Report) -> bool:
         """
         When a report exist for a changeset, download it and update redis data
         """
-        assert isinstance(report, Report)
-
         # Download the report
-        if not self.download_report(report):
+        if not download_report(self.reports_dir, self.bucket, report.name):
             logger.info("Report not available", report=str(report))
             return False
 
@@ -164,40 +162,6 @@ class GCPCache(object):
             )
 
         logger.info("Ingested report", report=str(report))
-        return True
-
-    def download_report(self, report):
-        """
-        Download and extract a json+zstd covdir report
-        """
-        assert isinstance(report, Report)
-
-        # Check the report is available on remote storage
-        blob = self.bucket.blob(report.gcp_path)
-        if not blob.exists():
-            logger.debug("No report found on GCP", path=report.gcp_path)
-            return False
-
-        if os.path.exists(report.path):
-            logger.info("Report already available", path=report.path)
-            return True
-
-        os.makedirs(os.path.dirname(report.archive_path), exist_ok=True)
-        blob.download_to_filename(report.archive_path)
-        logger.info("Downloaded report archive", path=report.archive_path)
-
-        with open(report.path, "wb") as output:
-            with open(report.archive_path, "rb") as archive:
-                dctx = zstd.ZstdDecompressor()
-                reader = dctx.stream_reader(archive)
-                while True:
-                    chunk = reader.read(16384)
-                    if not chunk:
-                        break
-                    output.write(chunk)
-
-        os.unlink(report.archive_path)
-        logger.info("Decompressed report", path=report.path)
         return True
 
     def find_report(
@@ -304,16 +268,17 @@ class GCPCache(object):
             for changeset, push in reports
         ]
 
-    def get_coverage(self, report, path):
+    def get_coverage(self, report: Report, path: str) -> dict:
         """
         Load a report and its coverage for a specific path
         and build a serializable representation
         """
-        assert isinstance(report, Report)
         data = covdir.open_report(report.path)
         if data is None:
             # Try to download the report if it's missing locally
-            assert self.download_report(report), "Missing report {}".format(report)
+            assert download_report(
+                self.reports_dir, self.bucket, report.name
+            ), "Missing report {}".format(report)
 
             data = covdir.open_report(report.path)
             assert data
@@ -363,41 +328,25 @@ class GCPCache(object):
 
         return [_coverage(changeset, date) for changeset, date in history]
 
-    def get_platforms(self, repository):
+    def get_platforms(self, repository: str) -> List[str]:
         """List all available platforms for a repository"""
         platforms = self.redis.smembers(KEY_PLATFORMS.format(repository=repository))
         return sorted(map(lambda x: x.decode("utf-8"), platforms))
 
-    def get_suites(self, repository):
+    def get_suites(self, repository: str) -> List[str]:
         """List all available suites for a repository"""
         suites = self.redis.smembers(KEY_SUITES.format(repository=repository))
         return sorted(map(lambda x: x.decode("utf-8"), suites))
 
-    def ingest_available_reports(self, repository, until=None):
+    def ingest_available_reports(
+        self, repository: str, until: Optional[datetime] = None
+    ) -> None:
         """
         Ingest all the available reports for a repository
         """
         assert isinstance(repository, str)
 
-        REGEX_BLOB = re.compile(
-            r"^{}/(\w+)/([\w\-]+):([\w\-]+).json.zstd$".format(repository)
-        )
-        now = datetime.utcnow().replace(tzinfo=pytz.UTC)
-        for blob in self.bucket.list_blobs(prefix=repository):
-
-            if isinstance(until, timedelta) and (now - blob.time_created) >= until:
-                logger.debug(f"Skipping old blob {blob}")
-                continue
-
-            # Get changeset from blob name
-            match = REGEX_BLOB.match(blob.name)
-            if match is None:
-                logger.warn("Invalid blob found {}".format(blob.name))
-                continue
-            changeset = match.group(1)
-            platform = match.group(2)
-            suite = match.group(3)
-
+        for changeset, platform, suite in list_reports(self.bucket, repository, until):
             # Build report instance and ingest it
             report = Report(self.reports_dir, repository, changeset, platform, suite)
             self.ingest_report(report)
--- tools/code_coverage_tools/gcp.py
+++ tools/code_coverage_tools/gcp.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
+import os
+import re
+from datetime import datetime
+from datetime import timedelta
+from typing import Iterator
+from typing import Optional
+from typing import Tuple
+
+import pytz
+import structlog
+import zstandard
 from google.cloud import storage as gcp_storage
 from google.oauth2.service_account import Credentials
 
+logger = structlog.get_logger(__name__)
 
-def get_bucket(service_account):
+DEFAULT_FILTER = "all"
+
+
+def get_bucket(service_account: str) -> gcp_storage.bucket.Bucket:
     """
     Build a Google Cloud Storage client & bucket
     from Taskcluster secret
@@ -21,3 +36,67 @@ def get_bucket(service_account):
     client = gcp_storage.Client(project=creds.project_id, credentials=creds)
 
     return client.get_bucket(bucket)
+
+
+def get_name(repository: str, changeset: str, platform: str, suite: str) -> str:
+    return f"{repository}/{changeset}/{platform}:{suite}"
+
+
+def download_report(
+    base_dir: str, bucket: gcp_storage.bucket.Bucket, name: str
+) -> bool:
+    path = f"{name}.json"
+    archive_path = f"{name}.json.zstd"
+    full_archive_path = os.path.join(base_dir, archive_path)
+    full_path = os.path.join(base_dir, path)
+
+    blob = bucket.blob(archive_path)
+    if not blob.exists():
+        logger.debug("No report found on GCP", path=archive_path)
+        return False
+
+    if os.path.exists(full_path):
+        logger.info("Report already available", path=full_path)
+        return True
+
+    os.makedirs(os.path.dirname(full_archive_path), exist_ok=True)
+    blob.download_to_filename(full_archive_path)
+    logger.info("Downloaded report archive", path=full_archive_path)
+
+    with open(full_path, "wb") as output:
+        with open(full_archive_path, "rb") as archive:
+            dctx = zstandard.ZstdDecompressor()
+            reader = dctx.stream_reader(archive)
+            while True:
+                chunk = reader.read(16384)
+                if not chunk:
+                    break
+                output.write(chunk)
+
+    os.unlink(full_archive_path)
+    return True
+
+
+def list_reports(
+    bucket: gcp_storage.bucket.Bucket, repository: str, until: Optional[datetime] = None
+) -> Iterator[Tuple[str, str, str]]:
+    REGEX_BLOB = re.compile(
+        r"^{}/(\w+)/([\w\-]+):([\w\-]+).json.zstd$".format(repository)
+    )
+    now = datetime.utcnow().replace(tzinfo=pytz.UTC)
+    for blob in bucket.list_blobs(prefix=repository):
+        if isinstance(until, timedelta) and (now - blob.time_created) >= until:
+            logger.debug(f"Skipping old blob {blob}")
+            continue
+
+        # Get changeset from blob name
+        match = REGEX_BLOB.match(blob.name)
+        if match is None:
+            logger.warn("Invalid blob found {}".format(blob.name))
+            continue
+        changeset = match.group(1)
+        platform = match.group(2)
+        suite = match.group(3)
+
+        # Build report instance and ingest it
+        yield changeset, platform, suite
