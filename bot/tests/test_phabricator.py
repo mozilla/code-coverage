mozilla/code-coverage

commit 4d6092ad8450e3216751c9be8df834a992cf9824

bot: Start hgmo server only once, and not once per report

bot/code_coverage_bot/commit_coverage.py
@@ -45,44 +45,48 @@ def generate(repo_dir: str) -> None:
     ), "Missing GOOGLE_CLOUD_STORAGE secret"
     bucket = get_bucket(secrets[secrets.GOOGLE_CLOUD_STORAGE])
 
-    for changeset, platform, suite in list_reports(bucket, "mozilla-central"):
-        # We are only interested in "overall" coverage, not platform or suite specific.
-        if platform != DEFAULT_FILTER or suite != DEFAULT_FILTER:
-            continue
+    with hgmo.HGMO(repo_dir=repo_dir) as hgmo_server:
+        for changeset, platform, suite in list_reports(bucket, "mozilla-central"):
+            # We are only interested in "overall" coverage, not platform or suite specific.
+            if platform != DEFAULT_FILTER or suite != DEFAULT_FILTER:
+                continue
 
-        # We already have data for this commit.
-        if changeset in commit_coverage:
-            continue
+            # We already have data for this commit.
+            if changeset in commit_coverage:
+                continue
 
-        report_name = get_name("mozilla-central", changeset, platform, suite)
-        assert download_report("ccov-reports", bucket, report_name)
+            report_name = get_name("mozilla-central", changeset, platform, suite)
+            assert download_report("ccov-reports", bucket, report_name)
 
-        with open(os.path.join("ccov-reports", f"{report_name}.json"), "r") as f:
-            report = json.load(f)
+            with open(os.path.join("ccov-reports", f"{report_name}.json"), "r") as f:
+                report = json.load(f)
 
-        phabricatorUploader = PhabricatorUploader(repo_dir, changeset)
+            phabricatorUploader = PhabricatorUploader(repo_dir, changeset)
 
-        with hgmo.HGMO(repo_dir=repo_dir) as hgmo_server:
             changesets = hgmo_server.get_automation_relevance_changesets(changeset)
 
-        results = phabricatorUploader.generate(report, changesets)
-        for changeset in changesets:
-            desc = changeset["desc"].split("\n")[0]
+            results = phabricatorUploader.generate(report, changesets)
+            for changeset in changesets:
+                desc = changeset["desc"].split("\n")[0]
 
-            if any(text in desc for text in ["r=merge", "a=merge"]):
-                continue
+                if any(text in desc for text in ["r=merge", "a=merge"]):
+                    continue
 
-            # Lookup changeset coverage from phabricator uploader
-            coverage = results.get(changeset["node"])
-            if coverage is None:
-                logger.warn("No coverage found", changeset=changeset)
-                continue
+                # Lookup changeset coverage from phabricator uploader
+                coverage = results.get(changeset["node"])
+                if coverage is None:
+                    logger.warn("No coverage found", changeset=changeset)
+                    continue
 
-            commit_coverage[changeset["node"]] = {
-                "added": sum(c["lines_added"] for c in coverage["paths"].values()),
-                "covered": sum(c["lines_covered"] for c in coverage["paths"].values()),
-                "unknown": sum(c["lines_unknown"] for c in coverage["paths"].values()),
-            }
+                commit_coverage[changeset["node"]] = {
+                    "added": sum(c["lines_added"] for c in coverage["paths"].values()),
+                    "covered": sum(
+                        c["lines_covered"] for c in coverage["paths"].values()
+                    ),
+                    "unknown": sum(
+                        c["lines_unknown"] for c in coverage["paths"].values()
+                    ),
+                }
 
     cctx = zstandard.ZstdCompressor(threads=-1)
     with open(f"{commit_coverage_path}.zst", "wb") as zf:

commit cfcb5b446fbe6188d2343362f0b83e76e6ca6ef8

bot: Don't start the hgmo server for each coverage mapping
This is both an optimization (avoid restarting the hgmo server), and also a bug fix
for a regression introduced by 4d6092ad8450e3216751c9be8df834a992cf9824.
Indeed, after 4d6092ad8450e3216751c9be8df834a992cf9824, we had a hgmo server already
running, and so we couldn't start another one.

Fixes a regression from 4d6092ad8450e3216751c9be8df834a992cf9824

bot/code_coverage_bot/commit_coverage.py
@@ -67,7 +67,7 @@ def generate(repo_dir: str) -> None:
 
             changesets = hgmo_server.get_automation_relevance_changesets(changeset)
 
-            results = phabricatorUploader.generate(report, changesets)
+            results = phabricatorUploader.generate(hgmo_server, report, changesets)
             for changeset in changesets:
                 # Lookup changeset coverage from phabricator uploader
                 coverage = results.get(changeset["node"])

bot/code_coverage_bot/phabricator.py
@@ -152,7 +152,7 @@ class PhabricatorUploader(object):
         return ext[1:] in COVERAGE_EXTENSIONS
 
     def generate(
-        self, report: dict, changesets: List[dict]
+        self, hgmo_server: hgmo.HGMO, report: dict, changesets: List[dict]
     ) -> Dict[str, Dict[str, Any]]:
         results = {}
 
@@ -171,88 +171,86 @@ class PhabricatorUploader(object):
             set(sum((changeset["files"] for changeset in changesets), []))
         )
 
-        with hgmo.HGMO(self.repo_dir) as hgmo_server:
-            coverage_records_by_path = {
-                path: self._find_coverage(report, path) for path in all_paths
+        coverage_records_by_path = {
+            path: self._find_coverage(report, path) for path in all_paths
+        }
+
+        # Retrieve the annotate data for the build changeset.
+        build_annotate_by_path = {
+            path: hgmo_server.get_annotate(self.revision, path)
+            for path in all_paths
+            if coverage_records_by_path.get(path) is not None
+        }
+
+        for changeset in changesets:
+            # Retrieve the revision ID for this changeset.
+            revision_id = parse_revision_id(changeset["desc"])
+
+            results[changeset["node"]] = {
+                "revision_id": revision_id,
+                "paths": {},
             }
 
-            # Retrieve the annotate data for the build changeset.
-            build_annotate_by_path = {
-                path: hgmo_server.get_annotate(self.revision, path)
-                for path in all_paths
-                if coverage_records_by_path.get(path) is not None
-            }
+            # For each file...
+            for path in changeset["files"]:
+                # Retrieve the coverage data.
+                coverage_record = coverage_records_by_path.get(path)
+                if coverage_record is None:
+                    continue
 
-            for changeset in changesets:
-                # Retrieve the revision ID for this changeset.
-                revision_id = parse_revision_id(changeset["desc"])
+                # Retrieve the annotate data for the build changeset.
+                build_annotate = build_annotate_by_path.get(path)
+                if build_annotate is None:
+                    # This means the file has been removed by another changeset, but if this is the
+                    # case, then we shouldn't have a coverage record and so we should have *continue*d
+                    # earlier.
+                    assert (
+                        False
+                    ), "Failure to retrieve annotate data for the build changeset"
 
-                results[changeset["node"]] = {
-                    "revision_id": revision_id,
-                    "paths": {},
+                # Build the coverage map from the annotate data and the coverage data of the build changeset.
+                coverage_map = self._build_coverage_map(build_annotate, coverage_record)
+
+                # Retrieve the annotate data for the changeset of interest.
+                annotate = hgmo_server.get_annotate(changeset["node"], path)
+                if annotate is None:
+                    # This means the file has been removed by this changeset, and maybe was brought back by a following changeset.
+                    continue
+
+                # List lines added by this patch
+                lines_added = [
+                    line["lineno"] - 1
+                    for line in annotate
+                    if line["node"] == changeset["node"]
+                ]
+
+                # Apply the coverage map on the annotate data of the changeset of interest.
+                coverage = self._apply_coverage_map(annotate, coverage_map)
+
+                results[changeset["node"]]["paths"][path] = {
+                    "lines_added": sum(
+                        coverage[line] != "N"
+                        for line in lines_added
+                        if line < len(coverage)
+                    ),
+                    "lines_unknown": sum(
+                        coverage[line] == "X"
+                        for line in lines_added
+                        if line < len(coverage)
+                    ),
+                    "lines_covered": sum(
+                        coverage[line] == "C"
+                        for line in lines_added
+                        if line < len(coverage)
+                    ),
+                    "coverage": coverage,
                 }
-
-                # For each file...
-                for path in changeset["files"]:
-                    # Retrieve the coverage data.
-                    coverage_record = coverage_records_by_path.get(path)
-                    if coverage_record is None:
-                        continue
-
-                    # Retrieve the annotate data for the build changeset.
-                    build_annotate = build_annotate_by_path.get(path)
-                    if build_annotate is None:
-                        # This means the file has been removed by another changeset, but if this is the
-                        # case, then we shouldn't have a coverage record and so we should have *continue*d
-                        # earlier.
-                        assert (
-                            False
-                        ), "Failure to retrieve annotate data for the build changeset"
-
-                    # Build the coverage map from the annotate data and the coverage data of the build changeset.
-                    coverage_map = self._build_coverage_map(
-                        build_annotate, coverage_record
-                    )
-
-                    # Retrieve the annotate data for the changeset of interest.
-                    annotate = hgmo_server.get_annotate(changeset["node"], path)
-                    if annotate is None:
-                        # This means the file has been removed by this changeset, and maybe was brought back by a following changeset.
-                        continue
-
-                    # List lines added by this patch
-                    lines_added = [
-                        line["lineno"] - 1
-                        for line in annotate
-                        if line["node"] == changeset["node"]
-                    ]
-
-                    # Apply the coverage map on the annotate data of the changeset of interest.
-                    coverage = self._apply_coverage_map(annotate, coverage_map)
-
-                    results[changeset["node"]]["paths"][path] = {
-                        "lines_added": sum(
-                            coverage[line] != "N"
-                            for line in lines_added
-                            if line < len(coverage)
-                        ),
-                        "lines_unknown": sum(
-                            coverage[line] == "X"
-                            for line in lines_added
-                            if line < len(coverage)
-                        ),
-                        "lines_covered": sum(
-                            coverage[line] == "C"
-                            for line in lines_added
-                            if line < len(coverage)
-                        ),
-                        "coverage": coverage,
-                    }
 
         return results
 
-    def upload(self, report, changesets):
-        results = self.generate(report, changesets)
+    def upload(self, report: dict, changesets: List[dict]) -> Dict[str, Dict[str, Any]]:
+        with hgmo.HGMO(self.repo_dir) as hgmo_server:
+            results = self.generate(hgmo_server, report, changesets)
 
         if secrets[secrets.PHABRICATOR_ENABLED]:
             phabricator = PhabricatorAPI(

bot/tests/conftest.py
@@ -46,11 +46,8 @@ def commit(hg, diff_rev=None):
     return str(revision, "ascii")
 
 
-def changesets(repo_dir, revision):
-    from code_coverage_bot import hgmo
-
-    with hgmo.HGMO(repo_dir) as hgmo_server:
-        return hgmo_server.get_automation_relevance_changesets(revision)
+def changesets(hgmo_server, revision):
+    return hgmo_server.get_automation_relevance_changesets(revision)
 
 
 def load_file(path):

bot/tests/test_notifier.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from code_coverage_bot import hgmo
 from code_coverage_bot.notifier import notify_email
 from code_coverage_bot.phabricator import PhabricatorUploader
 from conftest import add_file
@@ -21,7 +22,8 @@ def test_notification(mock_secrets, mock_taskcluster, mock_phabricator, fake_hg_
 
     copy_pushlog_database(remote, local)
 
-    stack = changesets(local, revision2)
+    with hgmo.HGMO(local) as hgmo_server:
+        stack = changesets(hgmo_server, revision2)
     assert len(stack) == 2
     assert (
         stack[0]["desc"]
@@ -40,7 +42,8 @@ def test_notification(mock_secrets, mock_taskcluster, mock_phabricator, fake_hg_
         }
     )
     phab = PhabricatorUploader(local, revision2)
-    changesets_coverage = phab.generate(report, stack)
+    with hgmo.HGMO(local) as hgmo_server:
+        changesets_coverage = phab.generate(hgmo_server, report, stack)
 
     assert changesets_coverage == {
         revision1: {

bot/tests/test_phabricator.py
@@ -6,6 +6,7 @@ import urllib.parse
 
 import responses
 
+from code_coverage_bot import hgmo
 from code_coverage_bot.phabricator import PhabricatorUploader
 from conftest import add_file
 from conftest import changesets
@@ -28,7 +29,9 @@ def test_simple(mock_secrets, mock_phabricator, fake_hg_repo):
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1, 1, 0]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision))
+    with hgmo.HGMO(local) as hgmo_server:
+        stack = changesets(hgmo_server, revision)
+        results = phabricator.generate(hgmo_server, report, stack)
 
     assert results == {
         revision: {
@@ -44,7 +47,7 @@ def test_simple(mock_secrets, mock_phabricator, fake_hg_repo):
         }
     }
 
-    phabricator.upload(report, changesets(local, revision))
+    phabricator.upload(report, stack)
 
     assert len(responses.calls) >= 3
 
@@ -106,7 +109,10 @@ def test_file_with_no_coverage(mock_secrets, fake_hg_repo):
 
     phabricator = PhabricatorUploader(local, revision)
     report = covdir_report({"source_files": []})
-    results = phabricator.generate(report, changesets(local, revision))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision)
+        )
 
     assert results == {revision: {"revision_id": 1, "paths": {}}}
 
@@ -125,7 +131,10 @@ def test_one_commit_without_differential(mock_secrets, fake_hg_repo):
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1, 1, 0]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision)
+        )
 
     assert results == {
         revision: {
@@ -166,7 +175,10 @@ def test_two_commits_two_files(mock_secrets, fake_hg_repo):
             ]
         }
     )
-    results = phabricator.generate(report, changesets(local, revision2))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision2)
+        )
 
     assert results == {
         revision1: {
@@ -217,7 +229,10 @@ def test_changesets_overwriting(mock_secrets, fake_hg_repo):
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1, 1, 0]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision2))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision2)
+        )
 
     assert results == {
         revision1: {
@@ -266,7 +281,10 @@ def test_changesets_displacing(mock_secrets, fake_hg_repo):
             ]
         }
     )
-    results = phabricator.generate(report, changesets(local, revision2))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision2)
+        )
 
     assert results == {
         revision1: {
@@ -311,7 +329,10 @@ def test_changesets_reducing_size(mock_secrets, fake_hg_repo):
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision2))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision2)
+        )
 
     assert results == {
         revision1: {
@@ -359,7 +380,10 @@ def test_changesets_overwriting_one_commit_without_differential(
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1, 1, 0]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision2))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision2)
+        )
 
     assert results == {
         revision1: {
@@ -402,7 +426,10 @@ def test_removed_file(mock_secrets, fake_hg_repo):
 
     phabricator = PhabricatorUploader(local, revision2)
     report = covdir_report({"source_files": []})
-    results = phabricator.generate(report, changesets(local, revision2))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision2)
+        )
 
     assert results == {
         revision1: {"revision_id": 1, "paths": {}},
@@ -430,7 +457,10 @@ def test_backout_removed_file(mock_secrets, fake_hg_repo):
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1, 1, 0]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision3))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision3)
+        )
 
     assert results == {
         revision1: {
@@ -462,7 +492,10 @@ def test_coverable_last_lines(mock_secrets, mock_phabricator, fake_hg_repo):
     report = covdir_report(
         {"source_files": [{"name": "file", "coverage": [None, 0, 1, 1, 1]}]}
     )
-    results = phabricator.generate(report, changesets(local, revision))
+    with hgmo.HGMO(local) as hgmo_server:
+        results = phabricator.generate(
+            hgmo_server, report, changesets(hgmo_server, revision)
+        )
 
     assert results == {
         revision: {