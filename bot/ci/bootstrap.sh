b6fde96056d5c9279b10b039603f4812fa7d28c0
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-01-25 13:46:54 +0100

    bot: Update to the latest revision of version-control-tools

--- bot/ci/bootstrap.sh
+++ bot/ci/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 GRCOV_VERSION="v0.7.1"
 MERCURIAL_VERSION="5.6.1"
-VERSION_CONTROL_TOOLS_REV="29460de72118"
+VERSION_CONTROL_TOOLS_REV="388ea44236d7"
 
 apt-get update
 # libgoogle-perftools4 is currently required for grcov (until https://github.com/mozilla/grcov/issues/403 is fixed).
