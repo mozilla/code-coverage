2d08441317ae7e9edaa821a33ccead82f3553fb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-10-20 19:00:19 +0200

    bot: Update to Mercurial 6.8.1

--- bot/tests/test__system.py
+++ bot/tests/test__system.py
@@ -13,7 +13,7 @@ def test_mercurial():
 
     # Check mercurial version
     output = run_check(["hg", "version", "-T", "{ver}"])
-    assert output.decode("utf-8").startswith("6.5.2")
+    assert output.decode("utf-8").startswith("6.8.1")
 
     # Check needed extensions
     output = run_check(["hg", "version", "-T", "{extensions}"])
