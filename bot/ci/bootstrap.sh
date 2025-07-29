365a86de9c4c7acd693f431c023ff3779809a0e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-29 12:41:39 +0200

    bot: get tcmalloc grcov build as it is available again and has much better memory characteristics

--- bot/ci/bootstrap.sh
+++ bot/ci/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-GRCOV_VERSION="v0.10.1"
+GRCOV_VERSION="v0.10.3"
 MERCURIAL_VERSION="6.8.1"
 VERSION_CONTROL_TOOLS_REV="b93e1a67ef8e"
 
@@ -10,7 +10,7 @@ apt-get install --no-install-recommends -y gcc curl bzip2 python-dev-is-python3 
 pip install --disable-pip-version-check --quiet --no-cache-dir mercurial==$MERCURIAL_VERSION
 
 # Setup grcov
-curl -L https://github.com/mozilla/grcov/releases/download/$GRCOV_VERSION/grcov-x86_64-unknown-linux-musl.tar.bz2 | tar -C /usr/bin -xjv
+curl -L https://github.com/mozilla/grcov/releases/download/$GRCOV_VERSION/grcov-x86_64-unknown-linux-gnu-tc.tar.bz2 | tar -C /usr/bin -xjv
 chmod +x /usr/bin/grcov
 
 # Setup mercurial with needed extensions
