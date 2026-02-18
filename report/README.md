63df1a33278165405573ca5cdf769cb95d4644e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-18 17:53:09 +0100

    Remove 'report' as it was moved in firefox as a mach command
    See https://bugzilla.mozilla.org/show_bug.cgi?id=2017368

--- report/README.md
+++
@@ -1,3 +0,0 @@
-# firefox-code-coverage
-
-A description on how to use this tool is available at https://developer.mozilla.org/en-US/docs/Mozilla/Testing/Measuring_Code_Coverage_on_Firefox.
--- report/ci-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-pip install --disable-pip-version-check --no-cache-dir --quiet -r test-requirements.txt -r requirements.txt .
-
-python -m unittest discover tests
-python setup.py sdist bdist_wheel
-VERSION=$(cat VERSION)
-pip install dist/firefox_code_coverage-$VERSION.tar.gz
