b213b39320bac2a7e8faa2ccd9c796148f555eea
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-04 22:14:23 +0100

    Remove 'tools' subdirectory, merge into 'bot' (#2975)

--- bot/code_coverage_bot/__init__.py
+++ bot/code_coverage_bot/__init__.py
@@ -2,3 +2,27 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+COVERAGE_EXTENSIONS = [
+    # C
+    "c",
+    "h",
+    # C++
+    "cpp",
+    "cc",
+    "cxx",
+    "hh",
+    "hpp",
+    "hxx",
+    # JavaScript
+    "js",
+    "jsm",
+    "mjs",
+    "jsx",
+    "xul",
+    "xml",
+    "html",
+    "xhtml",
+    # Rust
+    "rs",
+]
