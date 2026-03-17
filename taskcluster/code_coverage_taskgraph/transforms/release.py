87833fcff89380b5bd9c95b0be73989e0d42a740
Johan Lorenzo <jlorenzo@mozilla.com>
2026-03-17 10:25:30 +0100

    Fix release kind: use level to detect dry-run instead of tasks_for

--- taskcluster/code_coverage_taskgraph/transforms/release.py
+++ taskcluster/code_coverage_taskgraph/transforms/release.py
@@ -8,16 +8,20 @@ def add_release_config(config, tasks):
     for task in tasks:
         params = config.params
         version = params["head_ref"]
+        is_level_3 = params["level"] == "3"
 
         task.setdefault("worker", {})
-        task["worker"]["command"] = [
-            "taskboot",
-            "github-release",
-            "mozilla/code-coverage",
-            version,
-        ]
-        task["worker"].setdefault("env", {})["TASKCLUSTER_SECRET"] = (
-            "project/relman/code-coverage/release"
-        )
+        command = ["taskboot", "github-release", "mozilla/code-coverage", version]
+        if not is_level_3:
+            command.append("--dry-run")
+        task["worker"]["command"] = command
+
+        if is_level_3:
+            task["worker"].setdefault("env", {})["TASKCLUSTER_SECRET"] = (
+                "project/relman/code-coverage/release"
+            )
+            task.setdefault("scopes", []).append(
+                "secrets:get:project/relman/code-coverage/release"
+            )
 
         yield task
